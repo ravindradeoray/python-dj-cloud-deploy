8c67a40a851266b6189142f5e2f78e1dc4987991
Ravi Deoray <ravindra.deoray@gmail.com>
2023-06-24 17:13:43 -0400

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,10 +149,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ## manage.py check --deploy fixes
 #SECURE_REDIRECT_EXEMPT = [r'^health/$', '/health', 'health/', 'health']
-if (not DEBUG):
-    SECURE_SSL_REDIRECT = False
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+# if (not DEBUG):
+#     SECURE_SSL_REDIRECT = False
+#     SESSION_COOKIE_SECURE = True
+#     CSRF_COOKIE_SECURE = True
 
 
 # Setup SSL 
